7fb20afe6c11bf7ffe084fbf9015aad44ba3b345
Lakshya Agrawal <lavi061997@gmail.com>
2018-02-04 15:14:30 +0530

    copy button functioning

--- copy-input.js
+++ copy-input.js
@@ -1,7 +1,26 @@
-let inputs = document.getElementsbyTagName('input');
+var inputs = document.getElementsByTagName('input');
+var btn = [];
+//inputs.classList.add("copy");
+for (var i = 0; i < inputs.length; i+=1){
 
-inputs.classList.add("copy");
+  var rect = inputs[i].getBoundingClientRect();
 
-inputs.forEach(input => {
-  console.log(input);
-})
+  btn[i] = document.createElement("BUTTON");
+  var buttonText = document.createTextNode("Copy");
+
+  btn[i].value = "copy";
+  btn[i].type = "button";
+  btn[i].index = i;
+  btn[i].appendChild(buttonText);
+
+  btn[i].onclick = function(){
+
+    /* Select the text field */
+    input = inputs[this.index];
+    input.select();
+    /* Copy the text inside the text field */
+    document.execCommand("Copy");
+}
+  inputs[i].parentNode.appendChild(btn[i]);
+
+}
